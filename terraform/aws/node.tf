ca2d1b22fd69815495f6e392a7645c7f58d51812
Artur Ferfecki <artur.ferfecki@sikt.no>
2022-08-12 13:11:25 +0200

    increasing number of nodes

--- terraform/aws/node.tf
+++ terraform/aws/node.tf
@@ -6,8 +6,8 @@ resource "aws_eks_node_group" "bergen" {
   instance_types  = ["t3.small"]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 3
+    max_size     = 4
     min_size     = 1
   }
 
